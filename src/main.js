195e0cba00e7075f73a2cc4e073b21df9fec319a
lijun <3403200271@qq.com>
2022-11-29 17:07:00 +0800

    初始化之引入vant

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
 // 导入全局样式
 import '@/styles/index.less'
-
+// 全局注册--------
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
